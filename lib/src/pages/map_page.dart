b2c24ebd21a1af5470fb9d0095b4b70b515b6dc1
Cristhoper Alvarez <acristhoper25@gmail.com>
2019-12-31 17:16:23 -0500

    change map type dynamic

--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:qr_reader_flutter_app/src/models/scan_model.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final mapController = MapController();
+  String mapType = 'streets';
+
   @override
   Widget build(BuildContext context) {
     final ScanModel scanModel = ModalRoute.of(context).settings.arguments;
@@ -13,16 +21,45 @@ class MapPage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location),
-            onPressed: () {},
+            onPressed: () {
+              mapController.move(scanModel.getLatLng(), 15);
+            },
           ),
         ],
       ),
-      body: _createFlutterMap(scanModel)
+      body: _createFlutterMap(scanModel),
+      floatingActionButton: _createFloatingButton(context),
+    );
+  }
+
+  Widget _createFloatingButton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        //streets, dark, light, outdoors, satellite
+
+        switch (mapType) {
+          case 'streets':
+            mapType = 'dark';break;
+          case 'dark':
+            mapType = 'light';break;
+          case 'light':
+            mapType = 'outdoors';break;
+          case 'outdoors':
+            mapType = 'satellite';break;
+          default:
+            mapType = 'streets';break;
+        }
+
+        setState(() {});
+      },
     );
   }
 
   Widget _createFlutterMap(ScanModel scanModel) {
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
         center: scanModel.getLatLng(),
         zoom: 15
@@ -40,7 +77,7 @@ class MapPage extends StatelessWidget {
             '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
         additionalOptions: {
           'accessToken': 'pk.eyJ1IjoiY3Jpc3Rob3BlcjI1IiwiYSI6ImNrNHVkd3pibDBoOGQza3FiemM1b3VxYnYifQ.2goXCIK41LPitgh38vemCg',
-          'id': 'mapbox.streets'
+          'id': 'mapbox.$mapType'
           //streets, dark, light, outdoors, satellite
         }
     );
